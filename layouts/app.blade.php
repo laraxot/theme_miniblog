0be0d95d66e4f99b21f9e4ccc3cd878b119dff1a
Malebestia <malebestia@yahoo.it>
2020-06-15 19:38:32 +0200

    .

--- layouts/app.blade.php
+++ layouts/app.blade.php
@@ -1,6 +1,7 @@
 @extends('pub_theme::layouts.plane')
 @section('body')
     <div class="site-wrapper">
+        @include('pub_theme::layouts.partials.headnav')
         @yield('content')
         @include('pub_theme::layouts.partials.footer')
     </div>
